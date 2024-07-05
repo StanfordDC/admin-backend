eed20cf01164b007f13005c149952785016ac663
StanfordDC <stanforddchandra@gmail.com>
2024-07-05 13:28:33 +0800

    Added get metrics feature

--- service/wastetypeResponse/routes.go
+++ service/wastetypeResponse/routes.go
@@ -18,7 +18,8 @@ func NewHandler(store types.WastetypeResponseStore) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router){
-	router.HandleFunc("/waste-type-response", h.handleGetAll).Methods("GET", "OPTIONS")
+	router.HandleFunc("/responses", h.handleGetAll).Methods("GET", "OPTIONS")
+	router.HandleFunc("/responses/metrics", h.handleGetMetrics).Methods("GET", "OPTIONS")
 }
 
 func (h *Handler) handleGetAll(w http.ResponseWriter, r *http.Request){
@@ -33,6 +34,31 @@ func (h *Handler) handleGetAll(w http.ResponseWriter, r *http.Request){
 		var item types.WastetypeResponse
 		doc.DataTo(&item)
 		items = append(items, item)
+	}
+	json.NewEncoder(w).Encode(items)
+}
+
+func (h *Handler) handleGetMetrics(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	iter := h.store.GetAll()
+	items := make(map[string]int)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+				break
+		}
+		items["feature"]++
+		var item types.WastetypeResponse
+		doc.DataTo(&item)
+		objects := item.Objects
+		for _, value := range objects{
+			if value == 1 {
+				items["goodResponse"]++
+			} else if value == 2 {
+				items["badResponse"]++
+			}
+			items["count"]++
+		} 
 	}
 	json.NewEncoder(w).Encode(items)
 }
