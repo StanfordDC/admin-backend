066fa0ae9cf4c4319b86c0b4320e4ee927d771dc
StanfordDC <stanforddchandra@gmail.com>
2024-07-08 16:22:26 +0800

    Added metrics history api

--- service/wastetypeResponse/routes.go
+++ service/wastetypeResponse/routes.go
@@ -4,7 +4,7 @@ import (
 	"admin-backend/types"
 	"encoding/json"
 	"net/http"
-
+	"fmt"
 	"github.com/gorilla/mux"
 	"google.golang.org/api/iterator"
 )
@@ -20,6 +20,7 @@ func NewHandler(store types.WastetypeResponseStore) *Handler {
 func (h *Handler) RegisterRoutes(router *mux.Router){
 	router.HandleFunc("/responses", h.handleGetAll).Methods("GET", "OPTIONS")
 	router.HandleFunc("/responses/metrics", h.handleGetMetrics).Methods("GET", "OPTIONS")
+	router.HandleFunc("/responses/history", h.handleGetHistory).Methods("GET","OPTIONS")
 }
 
 func (h *Handler) handleGetAll(w http.ResponseWriter, r *http.Request){
@@ -63,4 +64,47 @@ func (h *Handler) handleGetMetrics(w http.ResponseWriter, r *http.Request){
 		} 
 	}
 	json.NewEncoder(w).Encode(items)
+}
+
+func (h* Handler) handleGetHistory(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	iter := h.store.GetAll()
+	months := map[string]map[string]int{
+        "january":   {"good": 0, "bad": 0, "feature": 0},
+        "february":  {"good": 0, "bad": 0, "feature": 0},
+        "march":     {"good": 0, "bad": 0, "feature": 0},
+        "april":     {"good": 0, "bad": 0, "feature": 0},
+        "may":       {"good": 0, "bad": 0, "feature": 0},
+        "june":      {"good": 0, "bad": 0, "feature": 0},
+        "july":      {"good": 0, "bad": 0, "feature": 0},
+        "august":    {"good": 0, "bad": 0, "feature": 0},
+        "september": {"good": 0, "bad": 0, "feature": 0},
+        "october":   {"good": 0, "bad": 0, "feature": 0},
+        "november":  {"good": 0, "bad": 0, "feature": 0},
+        "december":  {"good": 0, "bad": 0, "feature": 0},
+    }
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+				break
+		}
+		var item types.WastetypeResponse
+		doc.DataTo(&item)
+
+		//Get the created month
+		month := doc.CreateTime.Month().String()
+		month = string(month[0]) + month[1:]
+		month = fmt.Sprintf("%s%s", string(month[0]|0x20), month[1:])
+
+		months[month]["feature"]++
+		objects := item.Objects
+		for _, value := range objects{
+			if value == 1 {
+				months[month]["good"]++
+			} else if value == 2 {
+				months[month]["bad"]++
+			}
+		} 
+	}
+	json.NewEncoder(w).Encode(months)
 }
