678384af81cbf2565ebc4a0296ba2cdc80fea5b5
StanfordDC <stanforddchandra@gmail.com>
2024-07-15 14:14:33 +0800

    Updated jwt expiration duration

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CreateJWT(secret []byte, id string) (string, error) {
-	expiration := time.Second * time.Duration(60*5)
+	expiration := time.Second * time.Duration(3600*24)
 	token :=  jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id": id,
 		"expiredAt": time.Now().Add(expiration).Unix(),
